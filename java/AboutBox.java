e4d281d2e933646664f96a6f6af3b41f815e0251
CEBA77 <CEBA1996@outlook.com>
2018-05-16 19:28:00 +0300

    Change the appearance of the "About" window
    It is for next version.

--- java/AboutBox.java
+++ java/AboutBox.java
@@ -21,22 +21,24 @@ package com.lushprojects.circuitjs1.client;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
 
-public class AboutBox extends PopupPanel {
+public class AboutBox extends DialogBox {
 	
 	VerticalPanel vp;
 	Button okButton;
+	CirSim sim;
 	
 	AboutBox(String version) {
 		super();
 		vp = new VerticalPanel();
 		setWidget(vp);
-		vp.setWidth("400px");
-		vp.add(new HTML("<p>Circuit Simulator version 1.0.0</p>"+
+		setText(sim.LS("About"));
+		vp.setWidth("500px");
+		vp.add(new HTML("<p>Circuit Simulator version 1.0.1</p>"+
 		"<p>Original by Paul Falstad.(http://www.falstad.com/)</p>"+
 		"<p>JavaScript conversion by Iain Sharp (version "+version+" for JavaScript).(http://lushprojects.com/)</p>"+
 		"<p>The program was ported to Windows, MAC OS and Linux by Usevalad Khatkevich (NW.js version 0.17.0).</p>"+
@@ -47,18 +49,18 @@ public class AboutBox extends PopupPanel {
 		"Krystian Sławiński for the Polish translation; " +
 		"Andre Adrian for improved emitter coupled oscillator; Felthry for many examples; " +
 		"Colin Howell for code improvements.</p>"+
-		"<p style=\"font-size:9px\">This program is free software: you can redistribute it and/or modify it "+
+		"<p style=\"font-size:10px\">This program is free software: you can redistribute it and/or modify it "+
 		"under the terms of the GNU General Public License as published by "+
 		"the Free Software Foundation, either version 2 of the License, or "+
 		"(at your option) any later version.</p>"+
-		"<p style=\"font-size:9px\">This program is distributed in the hope that it will be useful,"+
+		"<p style=\"font-size:10px\">This program is distributed in the hope that it will be useful,"+
 		"but WITHOUT ANY WARRANTY; without even the implied warranty of "+
 		"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "+
 		"GNU General Public License for more details.</p>"+
-		"<p style=\"font-size:9px\">For details of licensing see http://www.gnu.org/licenses/.</p>"+
-		"<p style=\"font-size:9px\">Source code (Paul): https://github.com/pfalstad/circuitjs1</p>"+
-		"<p style=\"font-size:9px\">Source code (Usevalad): https://github.com/pfalstad/circuitjs1</p>"+
-	    "<p style=\"font-size:9px\">Source code (Iain): https://github.com/sharpie7/circuitjs1</p>"));
+		"<p style=\"font-size:10px\">For details of licensing see http://www.gnu.org/licenses/.</p>"+
+		"<p style=\"font-size:10px\">Source code (Paul): https://github.com/pfalstad/circuitjs1</p>"+
+		"<p style=\"font-size:10px\">Source code (Iain): https://github.com/sharpie7/circuitjs1</p>"+
+	    "<p style=\"font-size:10px\">Source code (Usevalad): https://github.com/CEBA77/circuitjs1</p>"));
 		
 		
 		vp.add(okButton = new Button("OK"));
