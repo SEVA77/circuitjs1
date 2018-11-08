04083926b79c2a58c1daa87649f598bcefce4896
CEBA77 <CEBA1996@outlook.com>
2018-11-09 00:56:16 +0300

    Full change of the "About..." window.

--- java/AboutBox.java
+++ java/AboutBox.java
@@ -1,5 +1,5 @@
 /*    
-    Copyright (C) Paul Falstad, Iain Sharp
+    Copyright (C) Paul Falstad and Iain Sharp
     
     This file is part of CircuitJS1.
 
@@ -16,52 +16,27 @@
     You should have received a copy of the GNU General Public License
     along with CircuitJS1.  If not, see <http://www.gnu.org/licenses/>.
 */
-/*The file was changed for desktop program by Usevalad Khatkevich*/
+
 package com.lushprojects.circuitjs1.client;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
 
-public class AboutBox extends DialogBox {
+public class AboutBox extends PopupPanel {
 	
 	VerticalPanel vp;
 	Button okButton;
-	CirSim sim;
 	
 	AboutBox(String version) {
 		super();
 		vp = new VerticalPanel();
 		setWidget(vp);
-		setText(sim.LS("About..."));
-		vp.setWidth("500px");
-		vp.add(new HTML("<p>Circuit Simulator version 1.0.3</p>"+
-		"<p>Original by Paul Falstad.(<a href=\"http://www.falstad.com/\" target=\"_blank\">http://www.falstad.com/</a>)</p>"+
-		"<p>JavaScript conversion by Iain Sharp (version "+version+" for web application).(<a href=\"http://lushprojects.com/\" target=\"_blank\">http://lushprojects.com/</a>)</p>"+
-		"<p>The program was  compiled to desktop application by Usevalad Khatkevich (NW.js version 0.17.0).</p>"+
-		"<p style=\"font-size:11px\">Thanks to: Edward Calver for 15 new components and other improvements; Rodrigo Hausen for file import/export and many other UI improvements; "+  
-		"J. Mike Rollins for the Zener diode code; Julius Schmidt for the spark gap code and some examples; Dustin Soodak for help with the user interface improvements; "+
-		"Jacob Calvert for the T Flip Flop; Ben Hayden for scope spectrum; " +
-		"Thomas Reitinger for the German translation; " +
-		"Krystian Sławiński for the Polish translation; " +
-		"Usevalad Khatkevich for the Russian translation; " +
-		"Andre Adrian for improved emitter coupled oscillator; Felthry for many examples; " +
-		"Colin Howell for code improvements.</p>"+
-		"<p style=\"font-size:10px\">This program is free software: you can redistribute it and/or modify it "+
-		"under the terms of the GNU General Public License as published by "+
-		"the Free Software Foundation, either version 2 of the License, or "+
-		"(at your option) any later version.</p>"+
-		"<p style=\"font-size:10px\">This program is distributed in the hope that it will be useful,"+
-		"but WITHOUT ANY WARRANTY; without even the implied warranty of "+
-		"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "+
-		"GNU General Public License for more details.</p>"+
-		"<p style=\"font-size:10px\">For details of licensing see <a href=\"http://www.gnu.org/licenses/\" target=\"_blank\">http://www.gnu.org/licenses/</a>.</p>"+
-		"<p style=\"font-size:10px\">Source code (Paul): <a href=\"https://github.com/pfalstad/circuitjs1\" target=\"_blank\">https://github.com/pfalstad/circuitjs1</a></p>"+
-		"<p style=\"font-size:10px\">Source code (Iain): <a href=\"https://github.com/sharpie7/circuitjs1\" target=\"_blank\">https://github.com/sharpie7/circuitjs1</a></p>"+
-	    "<p style=\"font-size:10px\">Source code (Usevalad): <a href=\"https://github.com/CEBA77/circuitjs1\" target=\"_blank\">https://github.com/CEBA77/circuitjs1</a></p>"));
+		vp.setWidth("400px");
+		vp.add(new HTML("<iframe src=\"help/aboutbox.html\" width=\"400\" height=\"430\" scrolling=\"auto\" frameborder=\"0\"></iframe><br>"));
 		
 		
 		vp.add(okButton = new Button("OK"));
