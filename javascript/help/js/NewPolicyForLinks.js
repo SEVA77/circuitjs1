fb6c0b4d168f26af3151caaa05a1c43c1fc616ca
CEBA77 <CEBA1996@outlook.com>
2018-11-21 08:47:16 +0300

    New policy for some links
    “Print ...” and instructions for custom logic and elements of active building blocks are working now.

--- javascript/help/js/NewPolicyForLinks.js
+++
@@ -1,7 +0,0 @@
-nw.Window.get().on('new-win-policy', function(frame, url, policy) {
-    // do not open the window
-    policy.ignore();
-    // and open it in external browser
-    nw.Shell.openExternal(url);
-  })
-  // This script is from http://docs.nwjs.io/en/latest/References/Window/#event-new-win-policy-frame-url-policy
