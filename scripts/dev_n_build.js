86ce6dd969e6710bd492c0e7d8f2db2e15721cca
Usevalad Khatkevich <CEBA1996@outlook.com>
2024-03-10 14:10:06 +0300

    add check of all steps

--- scripts/dev_n_build.js
+++ scripts/dev_n_build.js
@@ -1,22 +1,49 @@
 const { statSync } = require('node:fs');
 //const { version } = require('os');
 
-const stepNames = [ // Name of steps
+//const nw_version = '0.64.1-mod1';
+const nw_version = '0.20.0';
+
+const stepNames = [
     "Devmode (mvn gwt:devmode)",
-    "WebApp (mvn clean install)"
+    "WebApp (mvn clean install)",
+    "├── nwjs-v"+nw_version+"-linux-ia32.tar.gz",
+    "├── nwjs-v"+nw_version+"-linux-x64.tar.gz",
+    "├── nwjs-v"+nw_version+"-osx-arm64.zip",
+    "├── nwjs-v"+nw_version+"-osx-x64.zip",
+    "├── nwjs-v"+nw_version+"-win-ia32.zip",
+    "└── nwjs-v"+nw_version+"-win-x64.zip",
+    "├── linux-ia32",
+    "├── linux-x64",
+    "├── osx-arm64",
+    "├── osx-x64",
+    "├── win-ia32",
+    "└── win-x64"
 ]
 
 const pathsToCheck = [
     './war/circuitjs1/setuplist.txt',
-    './target/site/circuitjs1/setuplist.txt'
+    './target/site/circuitjs1/setuplist.txt',
+    './nwjs_cache/nwjs-v'+nw_version+'-linux-ia32',
+    './nwjs_cache/nwjs-v'+nw_version+'-linux-x64',
+    './nwjs_cache/nwjs-v'+nw_version+'-osx-arm64',
+    './nwjs_cache/nwjs-v'+nw_version+'-osx-x64',
+    './nwjs_cache/nwjs-v'+nw_version+'-win-ia32',
+    './nwjs_cache/nwjs-v'+nw_version+'-win-x64',
+    './out/linux-ia32',
+    './out/linux-x64',
+    './out/osx-arm64',
+    './out/osx-x64',
+    './out/win-ia32',
+    './out/win-x64'
 ]
 
-let stateOfSteps = new Array(2); // State of steps
-let diffTimes_ms = new Array(2); // diff between mtime and current time
+let stateOfSteps = new Array(13);
+let diffTimes_ms = new Array(13); // diff between mtime and current time
 
 function checkSteps() {
 
-    let lastStep = -1;
+    lastStep = -1;
     let lastStepTime = -1;
     let currentTime = new Date();
 
@@ -58,15 +85,31 @@ function checkSteps() {
         }
     }
 
+    let x = stateOfSteps;
+    isDownloadComplite = x[2]&x[3]&x[4]&x[5]&x[6]&x[7];
+    isLastBinaryComplite = x[8]&x[9]&x[10]&x[11]&x[12]&x[13];
+
+    const completedInfo = "[✔] ";
+    const notCompletedInfo = "[✘] ";
+
     console.log("Check steps:");
     for (let i = 0; i < stepNames.length; i++) {
         let getLastСhangeInfo = (i==lastStep) ? " ◀- LAST" : "";
-        let getStepCompliteInfo = (stateOfSteps[i]) ? "[✔] " : "[✘] ";
-        let basicInfo = getStepCompliteInfo+stepNames[i];
+        let getCompletedStateInfo = (stateOfSteps[i]) ? completedInfo : notCompletedInfo;
+        let basicInfo = getCompletedStateInfo+stepNames[i];
         if (stateOfSteps[i])
             console.info(basicInfo+" - "+getTimeAgoInfo(diffTimes_ms[i])+getLastСhangeInfo)
         else
             console.warn(basicInfo)
+        if (i==1 || i==7){
+            let isCompleted = (i==1) ? isDownloadComplite : isLastBinaryComplite;
+            let getCompletedInfo = (isCompleted) ? completedInfo : notCompletedInfo;
+            let basicInfo = (i==1) ?
+            getCompletedInfo+"GET NW.JS BINARIES" :
+            getCompletedInfo+"BUILD CIRCUITJS1 DESKTOP MOD";
+            if (isCompleted) console.info(basicInfo)
+            else console.warn(basicInfo);
+        }
     }
 
 }
