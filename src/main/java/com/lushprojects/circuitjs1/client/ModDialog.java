d61e2246c5f966ab2a4c28f5fc8b6d0c1608b82a
Usevalad Khatkevich <CEBA1996@outlook.com>
2024-05-14 02:58:10 +0300

    Add UI scale setting to ModDialog

--- src/main/java/com/lushprojects/circuitjs1/client/ModDialog.java
+++ src/main/java/com/lushprojects/circuitjs1/client/ModDialog.java
@@ -11,20 +11,100 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Button;
 import com.lushprojects.circuitjs1.client.util.Locale;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.core.client.ScriptInjector;
 
 public class ModDialog extends DialogBox {
 	
 	VerticalPanel vp;
+	
+	HorizontalPanel scaleButtons;
+	Button setScale100Button;
+	Button setDefaultScaleButton;
+	Button setScaleButton;
+	HTML scaleScrollbarElm;
+	native float getRealScale() /*-{
+		return $wnd.nw.Window.get().zoomLevel;
+	}-*/;
+	native float getDefaultScale() /*-{
+		return $wnd.defaultScale;
+	}-*/;
+	String getScaleScrollbar(float value,int scale){
+		return "<input type=\"range\" id=\"scaleUI\" oninput=\"getScaleInfo()\"" +
+			"min=\"-0.5\" max=\"2\" step=\"0.1\" value=\""+value+"\"" +
+			"style=\"width:455px\"><b><span class=\"scaleInfo\"" +
+			"style=\"vertical-align:super\">"+scale+"%</span></b>";
+	}
+
 	Button okButton;
 	CirSim sim;
-	
+
 	ModDialog() {
+
 		super();
 		vp = new VerticalPanel();
 		setWidget(vp);
 		setText("Modification Setup");
 		vp.setWidth("500px");
-		vp.add(new HTML("<h1>TEST</h1>"));
+
+		vp.add(new HTML("<big><b>UI scale:</b></big>"));
+		vp.add(scaleScrollbarElm = new HTML(getScaleScrollbar(getRealScale(),(int)(getRealScale()*100+100))));
+		vp.setCellHorizontalAlignment(scaleScrollbarElm, HasHorizontalAlignment.ALIGN_CENTER);
+		vp.add(scaleButtons = new HorizontalPanel());
+		scaleButtons.setWidth("100%");
+		scaleButtons.add(setScale100Button = new Button("100%",
+			new ClickHandler(){
+				public void onClick(ClickEvent event) {
+					vp.remove(scaleScrollbarElm);
+					vp.insert(scaleScrollbarElm = new HTML(getScaleScrollbar(0,100)),1);
+					vp.setCellHorizontalAlignment(scaleScrollbarElm, HasHorizontalAlignment.ALIGN_CENTER);
+					ScriptInjector.fromString("setScaleUI()")
+						.setWindow(ScriptInjector.TOP_WINDOW)
+						.inject();
+					//TODO: Save data to localStorage
+				}
+			}));
+		scaleButtons.add(setDefaultScaleButton = new Button("Default scale<b>*</b>",
+			new ClickHandler() {
+				public void onClick(ClickEvent event) {
+					vp.remove(scaleScrollbarElm);
+					vp.insert(scaleScrollbarElm = new HTML(getScaleScrollbar(getDefaultScale(),
+						(int)(getDefaultScale()*100+100))),1);
+					vp.setCellHorizontalAlignment(scaleScrollbarElm, HasHorizontalAlignment.ALIGN_CENTER);
+					ScriptInjector.fromString("setScaleUI()")
+						.setWindow(ScriptInjector.TOP_WINDOW)
+						.inject();
+					//TODO: Save data to localStorage
+				}
+			}));
+		scaleButtons.add(setScaleButton = new Button("Set",
+			new ClickHandler() {
+				public void onClick(ClickEvent event) {
+					ScriptInjector.fromString("setScaleUI()")
+						.setWindow(ScriptInjector.TOP_WINDOW)
+						.inject();
+					//TODO: Save data to localStorage
+				}
+			}));
+		vp.add(new HTML("<p>* - the default UI scale for your monitor is set to "+
+			(int)(getDefaultScale()*100+100)+"%</p>"));
+		// Styling buttons:
+		setScaleButton.addStyleName("modButtons"); //.setHeight("20px");
+		setScaleButton.addStyleName("modSetButtons");
+		setScale100Button.addStyleName("modButtons");
+		setDefaultScaleButton.addStyleName("modButtons");
+		//remove "gwt-Button" style:
+		setScaleButton.removeStyleName("gwt-Button");
+		setScale100Button.removeStyleName("gwt-Button");
+		setDefaultScaleButton.removeStyleName("gwt-Button");
+		scaleButtons.setCellHorizontalAlignment(setScale100Button, HasHorizontalAlignment.ALIGN_CENTER);
+		scaleButtons.setCellHorizontalAlignment(setDefaultScaleButton, HasHorizontalAlignment.ALIGN_CENTER);
+		scaleButtons.setCellHorizontalAlignment(setScaleButton, HasHorizontalAlignment.ALIGN_CENTER);
+		/*
+		vp.add(new HTML("<hr><big><b>Start/Stop and Reset buttons:</b></big>"));
+		vp.add(new HTML("<big><big>(in developing)</big></big>"));
+		*/
 		vp.add(okButton = new Button("OK"));
 		okButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
@@ -38,4 +118,5 @@ public class ModDialog extends DialogBox {
 	protected void closeDialog() {
 		hide();
 	}
+
 }
--- war/js/sys.js
+++ war/js/sys.js
@@ -23,18 +23,34 @@ let dheight =  nw.Screen.screens[0].bounds.height; // for new window
 Scale: nw.Window.get().zoomLevel
 */
 
-  if (dwidth >= 1960)
-    nw.Window.get().zoomLevel = 1.6; // 2-0.4 and etc.
-  else if (dwidth >= 1752 && dwidth < 1960)
-    nw.Window.get().zoomLevel = 1.1; // -0.4
-  else if (dwidth >= 1600 && dwidth < 1752)
-    nw.Window.get().zoomLevel = 0.7; // -0.3
-  else if (dwidth >= 1460 && dwidth < 1600)
-    nw.Window.get().zoomLevel = 0.3; // -0.2
-  else if (dwidth >= 1200 && dwidth < 1460)
-    nw.Window.get().zoomLevel = -0.1; // -0.1
-  else if (dwidth < 1200)
-    nw.Window.get().zoomLevel = -0.3;
+var defaultScale;
+
+if (dwidth >= 1960)
+  defaultScale = 1.6; // 2-0.4 and etc.
+else if (dwidth >= 1752 && dwidth < 1960)
+  defaultScale = 1.1; // -0.4
+else if (dwidth >= 1600 && dwidth < 1752)
+  defaultScale = 0.7; // -0.3
+else if (dwidth >= 1460 && dwidth < 1600)
+  defaultScale = 0.3; // -0.2
+else if (dwidth >= 1200 && dwidth < 1460)
+  defaultScale = -0.1; // -0.1
+else if (dwidth < 1200)
+  defaultScale = -0.3;
+
+//TODO: add condition when implementing the localstorage info about scale
+nw.Window.get().zoomLevel = defaultScale;
+
+function setScaleUI(){
+  let scale = document.getElementById("scaleUI").value;
+  nw.Window.get().zoomLevel = parseFloat(scale);
+}
+
+function getScaleInfo(){
+  let scaleString = document.querySelector('.scaleInfo');
+  let scale = document.getElementById("scaleUI").value;
+  scaleString.textContent = parseInt(scale*100+100)+"%";
+}
 
 //use dwidth and dheight variables for size new window
 ////nw.Window.get().resizeTo(dwidth*0.75, dheight*0.75);
