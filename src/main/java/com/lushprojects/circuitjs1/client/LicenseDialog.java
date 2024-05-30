2a9a95defe5fde926aa1eb7fedc3df715218c379
Usevalad Khatkevich <CEBA1996@outlook.com>
2024-05-30 23:01:38 +0300

    add required license notice

--- src/main/java/com/lushprojects/circuitjs1/client/LicenseDialog.java
+++ src/main/java/com/lushprojects/circuitjs1/client/LicenseDialog.java
@@ -1,5 +1,20 @@
 /*    
-    Copyright (C) Usevalad Khatkevich
+    Copyright (C) Paul Falstad and Usevalad Khatkevich
+    
+    This file is part of CircuitJS1.
+
+    CircuitJS1 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    CircuitJS1 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CircuitJS1.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 package com.lushprojects.circuitjs1.client;
